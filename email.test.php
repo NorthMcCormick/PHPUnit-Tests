0f00990d41ead7dadf7a9a89da9faf43852521cc
Mike Reed <mike.reed.vallejo@gmail.com>
2017-10-01 12:56:52 -0700

    Update email.test.php
    Test 'testEmailSend' stubs two methods in the Email class  ("logMail","phpMail").
    
    I stubbed the phpMail meethod because my test environment did not include an email server, so the call to Mail() was failing.  In order to make the stub possible, I changed the phpMail() method definition in email.php from "private" to "protected".

--- email.test.php
+++ email.test.php
@@ -13,15 +13,23 @@ class EmailTest extends TestCase
    */
   public function testCanBeCreatedFromValidEmailAddress()
   {
-
+    $emailStr = "mike.reed.vallejo@gmail.com";
+    
+    $emailClassObj = new Email($emailStr);
+    $this->assertInstanceOf("Email", $emailClassObj);
   }
 
   /**
    * Will this email class fail to be instantiated with an invalid email address?
    */
+  /**
+   * @expectedException InvalidArgumentException
+   */
   public function testCannotBeCreatedFromInvalidEmailAddress()
   {
-
+    $emailStr = "This-is-an-invalid-email-string";
+    
+    $emailClassObj = new Email($emailStr);
   }
 
   /**
@@ -29,7 +37,11 @@ class EmailTest extends TestCase
    */
   public function testCanBeUsedAsString()
   {
-
+    $emailStr = "mike.reed.vallejo@gmail.com";
+    
+    $emailClassObj = new Email($emailStr);
+    $result = $emailClassObj->fromString($emailStr);
+    $this->assertTrue($emailStr == $result);
   }
 
   /**
@@ -37,6 +49,20 @@ class EmailTest extends TestCase
    * that we can override the log function. Logging the mail exits the application so we need to bypass that.
    */
   public function testEmailSend() {
+    $emailStr = "mike.reed.vallejo@gmail.com";
+
+    $emailMock = $this->getMockBuilder("Email")
+      ->setConstructorArgs(array($emailStr))
+      ->setMethods(array("logMail","phpMail"))
+      ->getMock();
+
+    $emailMock->expects($this->once())
+      ->method("logMail");
+    $emailMock->expects($this->once())
+      ->method("phpMail");
+
+    $emailMock->sendEmail();
 
   }
+
 }
